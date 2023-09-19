8fd60e0efd6354dae18e8c1f64643e72b3ad1091
altertnative <144519154+alternative00@users.noreply.github.com>
2023-09-19 16:35:15 +0900

    Update ProfileEditor.tsx

--- src/components/ProfileEditor.tsx
+++ src/components/ProfileEditor.tsx
@@ -115,7 +115,7 @@ export function ProfileEditor(props: ProfileEditorProps): JSX.Element {
                         updateProfile()
                     }}
                 >
-                    {props.id === undefined ? '글쓰기' : '업데이트'}
+                    {props.id === undefined ? 'New' : '업데이트'}
                 </Button>
             </Box>
         </Box>
